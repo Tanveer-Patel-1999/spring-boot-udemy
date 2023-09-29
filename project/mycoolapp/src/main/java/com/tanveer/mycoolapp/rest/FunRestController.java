8883843f326bffdbe8f958549e29de31645835ae
Tanveer <tanveerpatet1999@gmail.com>
2023-09-29 17:54:10 +0530

    second day

--- project/mycoolapp/src/main/java/com/tanveer/mycoolapp/rest/FunRestController.java
+++ project/mycoolapp/src/main/java/com/tanveer/mycoolapp/rest/FunRestController.java
@@ -1,13 +1,31 @@
 package com.tanveer.mycoolapp.rest;
 
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
-    @GetMapping()
+    @Value("${developer.name}")
+    private String name;
+    @Value("${team.name}")
+    private String team;
+
+    @GetMapping("/hii")
     public String sayHello(){
         return "HELLO SPRING BOOT....";
     }
+
+    @GetMapping("/bye")
+    public String sayBye(){
+        return "BYE SPRING BOOT....123";
+    }
+
+    @GetMapping("/team")
+    public String teamInfo(){
+            return "NAME :"+ name + ",TEAM : "+ team;
+    }
+
 }
